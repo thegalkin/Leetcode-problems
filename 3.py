a1912dc72fdc66c4f0588b2a3a1e0d231a80dbe2
theGalkin <53794828+thegalkin@users.noreply.github.com>
2020-08-29 14:19:19 +0300

    --- Auto-Git Commit ---

--- 3.py
+++ 3.py
@@ -1,30 +1,28 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        maximum = 0
-        maxstr = ""
-        for i in s: #ord order 
-            a = s[s.find(i):len(s)+1] #saving string to check it
-            for j in a: # checking the string
-                if not a.count(j) < 2:
-                    a = ""
-                    break
-            if a == "":
-                pass
-            if len(a) > maximum:
-                maximum = len(a)
-                maxstr = a
-        s = s[::-1]
-        for i in s: #rev order
-            a = s[s.find(i):len(s)+1] #saving string to check it
-            for j in a: # checking the string
-                if not a.count(j) < 2:
-                    a = ""
-                    break
-            if a == "":
-                pass
-            if len(a) > maximum:
-                maximum = len(a)
-                maxstr = reversed(a)
-        return maximum
-sol = Solution
-sol.lengthOfLongestSubstring("abcdee")
+def length(s):
+    maximum = 0
+    maxstr = ""
+    for i in s: #ord order 
+        a = s[s.find(i):len(s)+1] #saving string to check it
+        for j in a: # checking the string
+            if not a.count(j) < 2:
+                a = ""
+                break
+        if a == "":
+            pass
+        if len(a) > maximum:
+            maximum = len(a)
+            maxstr = a
+    s = s[::-1]
+    for i in s: #rev order
+        a = s[s.find(i):len(s)+1] #saving string to check it
+        for j in a: # checking the string
+            if not a.count(j) < 2:
+                a = ""
+                break
+        if a == "":
+            pass
+        if len(a) > maximum:
+            maximum = len(a)
+            maxstr = reversed(a)
+    return maximum
+print(length("abcabcbb"))
